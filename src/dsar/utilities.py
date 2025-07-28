8284483d3e0db8b62cf7b83635612b686583710b
Martanto <martanto@live.com>
2025-07-28 10:18:23 +0200

    Refactor src directory

--- src/dsar/utilities.py
+++ src/dsar/utilities.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 def fill_streams(client: Client, station: str, date: UTCDateTime) -> Stream:
     """Return stream from SDS Directory based on network, station, location and channel"""
-    _network, _station, _location, _channel = station.split('.')
+    _network, _station, _location, _channel = station.split(".")
 
     stream = client.get_waveforms(
         network=_network,
@@ -17,20 +17,30 @@ def fill_streams(client: Client, station: str, date: UTCDateTime) -> Stream:
         location=_location,
         channel=_channel,
         starttime=date,
-        endtime=date + timedelta(days=1)
+        endtime=date + timedelta(days=1),
     )
 
     # Check if stream is not empty (files not found)
     # Return empty Stream if files are not found
     if stream.count():
-        stream_date: str = stream[0].stats.starttime.strftime('%Y-%m-%d')
-        if date.strftime('%Y-%m-%d') != stream_date:
-            print("⚠️ {} :: File(s) for date {} vs {} INVALID!".format(station, date.strftime('%Y-%m-%d'), stream_date))
+        stream_date: str = stream[0].stats.starttime.strftime("%Y-%m-%d")
+        if date.strftime("%Y-%m-%d") != stream_date:
+            print(
+                "⚠️ {} :: File(s) for date {} vs {} INVALID!".format(
+                    station, date.strftime("%Y-%m-%d"), stream_date
+                )
+            )
             return stream
-        print('ℹ️ {} :: File(s) for date {} OK!'.format(station, date.strftime('%Y-%m-%d')))
+        print(
+            "ℹ️ {} :: File(s) for date {} OK!".format(station, date.strftime("%Y-%m-%d"))
+        )
         return stream
     else:
-        print("⚠️ {} :: File(s) for date {} not found!".format(station, date.strftime('%Y-%m-%d')))
+        print(
+            "⚠️ {} :: File(s) for date {} not found!".format(
+                station, date.strftime("%Y-%m-%d")
+            )
+        )
         return Stream()
 
 
@@ -38,16 +48,14 @@ def trace_to_series(trace: Trace) -> pd.Series:
     index_time = pd.date_range(
         start=trace.stats.starttime.datetime,
         periods=trace.stats.npts,
-        freq="{}ms".format(trace.stats.delta * 1000)
+        freq="{}ms".format(trace.stats.delta * 1000),
     )
 
     _series = pd.Series(
-        data=np.abs(trace.data),
-        index=index_time,
-        name='values',
-        dtype=trace.data.dtype)
+        data=np.abs(trace.data), index=index_time, name="values", dtype=trace.data.dtype
+    )
 
-    _series.index.name = 'datetime'
+    _series.index.name = "datetime"
 
     return _series
 
@@ -57,8 +65,9 @@ def trace_to_dataframe(trace: Trace) -> pd.DataFrame:
 
 
 def calculate_per_band(frequencies: list[float], trace: Trace, corners=4) -> pd.Series:
-    trace = trace.filter('bandpass', freqmin=frequencies[0],
-                         freqmax=frequencies[-1], corners=corners)
+    trace = trace.filter(
+        "bandpass", freqmin=frequencies[0], freqmax=frequencies[-1], corners=corners
+    )
     return trace_to_series(trace)
 
 
@@ -66,16 +75,24 @@ def plot_continuous_eruption(axes, axvspans: list[list[str]]):
     for key, continuous in enumerate(axvspans):
         # continuous[0] = start date of eruption
         # continuous[1] = end date of eruption
-        axes.axvspan(continuous[0], continuous[1], alpha=0.4,
-                     color='orange', label="_" * key + 'Continuous Eruption')
+        axes.axvspan(
+            continuous[0],
+            continuous[1],
+            alpha=0.4,
+            color="orange",
+            label="_" * key + "Continuous Eruption",
+        )
 
     return axes
 
 
 def plot_single_eruption(axes, axvlines: list[str]):
     for key, date in enumerate(axvlines):
-        axes.axvline(datetime.strptime(date, '%Y-%m-%d'),
-                    color='red', label="_" * key + 'Single Eruption')
+        axes.axvline(
+            datetime.strptime(date, "%Y-%m-%d"),
+            color="red",
+            label="_" * key + "Single Eruption",
+        )
     return axes
 
 
@@ -90,8 +107,13 @@ def plot_eruptions(axes, axvspans: list[list[str]] = None, axvlines: list[str] =
 
 
 def get_combined_csv(directory: str, station: str, resample: str) -> pd.DataFrame:
-    df = pd.read_csv(os.path.join(
-        directory, station, 'combined_{}_{}.csv'.format(resample, station)),
-        index_col='datetime', parse_dates=True, date_format = '%Y-%m-%d %H:%M:%S')
+    df = pd.read_csv(
+        os.path.join(
+            directory, station, "combined_{}_{}.csv".format(resample, station)
+        ),
+        index_col="datetime",
+        parse_dates=True,
+        date_format="%Y-%m-%d %H:%M:%S",
+    )
 
     return df
